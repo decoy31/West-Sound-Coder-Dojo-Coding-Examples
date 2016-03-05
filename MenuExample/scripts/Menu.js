a440e72b6c7b07f67d93ae8c329d50c61d8f3aa5
Tim Duffey <tim.duffey@redfin.com>
2016-03-05 11:59:23 -0800

    Added some documentation Menu and created defaults for the ways to select the MenuItems.

--- MenuExample/scripts/Menu.js
+++ MenuExample/scripts/Menu.js
@@ -75,32 +75,72 @@ Menu.prototype = {
 		}
 	},
 
+	/**
+	 * Select a `MenuItem` by index.
+	 *
+	 * @param {number} [menuItemIndex=0] Index of the `MenuItem` to select.
+	 * @param {boolean} [stopUpdateMenu=false] If true, stops the HTML of the `Menu` from being updated. This is a flag
+	 *      used for performance so we don't constantly update the DOM in the browser.
+	 * @returns {MenuItem} Returns the selected `MenuItem`.
+	 */
 	selectItem: function (menuItemIndex, stopUpdateMenu) {
+		var selMenuItem;
 		var selMenuItemIndex = parseInt(menuItemIndex, 10) || 0;
 
-		if (isNaN(selMenuItemIndex) || selMenuItemIndex >= this.items.length || selMenuItemIndex < 0) {
+		if (isNaN(selMenuItemIndex) || selMenuItemIndex < 0) {
+			// If no valid menu index was given, select the first menu item.
+			selMenuItemIndex = 0;
+		} else if (selMenuItemIndex >= this.items.length) {
+			// If the given menu index is greater than the number of items in the menu, default to the last menu item.
 			selMenuItemIndex = this.items.length - 1;
 		}
 
 		for (var index = 0; index < this.items.length; index++) {
 			var menuItem = this.items[index];
 			menuItem.isSelected = index === selMenuItemIndex;
+
+			if (menuItem.isSelected) {
+				selMenuItem = menuItem;
+			}
 		}
 
 		if (!stopUpdateMenu) {
 			this._updateMenu();
 		}
+
+		return selMenuItem;
 	},
 
+	/**
+	 * Select a `MenuItem` by its URL.
+	 *
+	 * @param {string} [url] URL of the `MenuItem` to select.
+	 * @param {boolean} [stopUpdateMenu=false] If true, stops the HTML of the `Menu` from being updated. This is a flag
+	 *      used for performance so we don't constantly update the DOM in the browser.
+	 * @returns {MenuItem} Returns the selected `MenuItem`.
+	 */
 	selectItemByUrl: function (url, stopUpdateMenu) {
+		var selMenuItem;
+
 		for (var index = 0; index < this.items.length; index++) {
 			var menuItem = this.items[index];
 			menuItem.isSelected = menuItem.url === url;
+
+			if (menuItem.isSelected) {
+				selMenuItem = menuItem;
+			}
+		}
+
+		if (!selMenuItem) {
+			selMenuItem = this.items[0];
+			selMenuItem.isSelected = true;
 		}
 
 		if (!stopUpdateMenu) {
 			this._updateMenu();
 		}
+
+		return selMenuItem;
 	},
 
 	/**
@@ -158,10 +198,12 @@ Menu.prototype = {
  *
  * @param {Element} menuContainerElement
  * @param {Array<MenuItem>} [menuItems] Array of `MenuItems`.
+ * @param {number} [selectedMenuItemIndex] Index of the `MenuItem` to select when the `Menu` is initialized.
  * @returns {Menu} Returns the instance of `Menu` that gets created and initialized.
  */
-window.initMenu = function (menuContainerElement, menuItems) {
-	var menu = new Menu(menuItems);
+window.initMenu = function (menuContainerElement, menuItems, selectedMenuItemIndex) {
+	selectedMenuItemIndex = selectedMenuItemIndex || 0;
+	var menu = new Menu(menuItems, selectedMenuItemIndex);
 
 	// Add the menu to the container.
 	menuContainerElement.appendChild(menu.menuElement);
